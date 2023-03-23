e3b5c4c4d4b917c93a1c05d125fd22c5959f90a5
Simone <simone.giacomelli@gmail.com>
2023-03-23 20:40:32 +0000

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,54 @@
+use std::net::UdpSocket;
+use std::thread::Thread;
+use std::{thread, vec};
+
 mod find_udp_port;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+fn start_dns_server_thread(port: u16) {
+    thread::spawn(move || { start_dns_server(port) });
+}
+
+fn start_dns_server(port: u16) {
+    let bind = UdpSocket::bind(("0.0.0.0", port));
+    let mut buf: Vec<u8> = Vec::with_capacity(4096);
+    let (size, origin) = bind.unwrap().recv_from(&mut buf).unwrap();
+    println!("size={} origin={}", size, origin)
+}
+
 #[cfg(test)]
 mod tests {
     use std::process::Command;
+    use crate::find_udp_port::find_port;
     use super::*;
 
 
     #[test]
-    fn it_works_not() {
+    fn test_local_resolver() {
+        let port = find_port().unwrap();
+        start_dns_server_thread(port);
+
+        let output = Command::new("dig")
+            .args(format!("@127.0.0.1 -p {} example.com +short", port).split(" "))
+            .output()
+            .expect("failed to execute process");
+
+        println!("status: {}", output.status);
+        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+
+        let expected_ip = "1.1.1.1";
+        let actual_ip = String::from_utf8_lossy(&output.stdout).to_string();
+
+        assert!(output.status.success());
+        assert_eq!(actual_ip.trim(), expected_ip)
+    }
+
+    #[test]
+    fn dig_with_examplecom() {
         let mut cmd = Command::new("dig");
         let args1 = "@127.0.0.1 -p 10000 example.com +short";
         let args1 = "example.com +short";
