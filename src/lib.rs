24f336c2d7daff8e9e7ae1bd09abecb437c69bea
Simone <simone.giacomelli@gmail.com>
2023-03-23 20:16:09 +0000

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,12 @@ mod tests {
     use std::process::Command;
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let mut cmd = Command::new("dig");
-        let args1 = "@127.0.0.1 -p 10000 example.com +short";
-        // let args1 = "example.com";
-        args1.split(" ").for_each(|s| { cmd.arg(s); });
-        let mut execution = cmd.spawn().unwrap();
-        let result = execution.wait().unwrap();
-        assert_eq!(0, result.code().unwrap())
-    }
 
     #[test]
     fn it_works_not() {
         let mut cmd = Command::new("dig");
         let args1 = "@127.0.0.1 -p 10000 example.com +short";
-        // let args1 = "example.com";
-        args1.split(" ").for_each(|s| { cmd.arg(s); });
+        let args1 = "example.com +short";
 
         let output = Command::new("dig")
             .args(args1.split(" "))
@@ -31,9 +20,13 @@ mod tests {
             .expect("failed to execute process");
 
         println!("status: {}", output.status);
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
         println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+
+        let expected_ip = "93.184.216.34";
+        let actual_ip = String::from_utf8_lossy(&output.stdout).to_string();
 
         assert!(output.status.success());
+        assert_eq!(actual_ip.trim(), expected_ip)
     }
 }
