06097a2ceaefd91d9b5755da9e7b461a07f2c318
Simone <simone.giacomelli@gmail.com>
2023-03-25 17:08:03 +0000

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,11 @@ fn start_dns_server(port: u16) {
     }
 }
 
-fn decode_domain(buf: &[u8]) -> Result<Vec<String>, String> {
+struct Question {
+    qname: Vec<String>,
+}
+
+fn decode_questions(buf: &[u8]) -> Result<Vec<Question>, String> {
     let mut index = 0;
     let mut result: Vec<String> = Vec::new();
     loop {
@@ -31,10 +35,19 @@ fn decode_domain(buf: &[u8]) -> Result<Vec<String>, String> {
         if length == 0 { break; };
         index += 1;
         let slice = &buf[index..(index + length)];
-        result.push(std::str::from_utf8(&slice).unwrap().to_string());
+        let qname = std::str::from_utf8(&slice).unwrap().to_string();
+        result.push(qname);
         index += length;
     }
-    return Ok(result);
+    return Ok(vec![Question { qname: result }]);
+}
+
+struct Query {
+    questions: Vec<Question>,
+}
+
+fn decode_query(buf: &[u8]) -> Query {
+    Query { questions: decode_questions(&buf[12..]).unwrap() }
 }
 
 #[cfg(test)]
@@ -43,7 +56,7 @@ mod tests {
     use crate::find_udp_port::find_port;
     use super::*;
 
-    fn packet_example_com() -> Vec<u8> { Vec![0xE2, 0x61, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x65, 0x78, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x03, 0x63, 0x6F, 0x6D, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x29, 0x04, 0xD0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x00, 0x0A, 0x00, 0x08, 0x1D, 0xB6, 0x2D, 0x09, 0x30, 0xD8, 0x1A, 0xFB] }
+    fn packet_example_com() -> Vec<u8> { vec![0xE2, 0x61, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x65, 0x78, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x03, 0x63, 0x6F, 0x6D, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x29, 0x04, 0xD0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x00, 0x0A, 0x00, 0x08, 0x1D, 0xB6, 0x2D, 0x09, 0x30, 0xD8, 0x1A, 0xFB] }
 
     #[test]
     fn test_local_resolver() {
@@ -67,7 +80,7 @@ mod tests {
     }
 
     #[test]
-    fn dig_with_examplecom() {
+    fn dig_with_example_com() {
         let mut cmd = Command::new("dig");
         let args1 = "@127.0.0.1 -p 10000 example.com +short";
         let args1 = "example.com +short";
@@ -91,8 +104,20 @@ mod tests {
     #[test]
     fn test_decode_domain() {
         let buf: Vec<u8> = vec![0x2, 'x' as u8, 'y' as u8, 0x1, 'z' as u8, 0x0];
-        let actual = decode_domain(&buf).unwrap();
-        let expected = vec!["xy".to_string(), "z".to_string()];
+        let questions = decode_questions(&buf).unwrap();
+        assert_eq!(1, questions.len());
+        let actual = &questions[0].qname;
+        let expected = &vec!["xy".to_string(), "z".to_string()];
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_decode_query() {
+        let query = decode_query(&packet_example_com());
+        let questions = query.questions;
+        assert_eq!(1, questions.len());
+        let actual = &questions[0].qname;
+        let expected = &vec!["example".to_string(), "com".to_string()];
         assert_eq!(expected, actual)
     }
 }
